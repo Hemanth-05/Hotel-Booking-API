d6fdfe5a24c818e00d9d31304d8b2193c83dc737
Hemanth-05 <hemanthnath123@gmail.com>
2025-11-10 18:15:31 -0500

    Setting up user and auth Routes

--- src/server.js
+++ src/server.js
@@ -3,8 +3,29 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.json({message: "Welcome to the project"})
+if (process.env.NODE_ENV === 'development') {
+  const morganModule = await import('morgan');
+  const morgan = morganModule.default;
+  app.use(morgan('tiny'));
+}
+
+app.use(express.json());
+app.use('api/auth', authRoutes);
+app.use('api/users', userRoutes);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  if (!err.status) {
+    err.status = 500;
+    err.message = 'Internal Server Error';
+  }
+  res.status(err.status).json({ error: err.message });
 });
 
 if (process.env.NODE_ENV !== 'test') {
