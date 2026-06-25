4491c89cbc00beebb71f2df99b5a674d72e76433
Hemanth-05 <hemanthnath123@gmail.com>
2026-06-24 20:24:33 -0400

    Deployment

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(specs));
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
 }));
 
 //Root route
